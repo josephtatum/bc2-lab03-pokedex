f22c62a4ad2137b33e45289d0b87988d615ac1c0
Joseph Tatum <josephtatum@gmail.com>
2019-11-01 14:14:54 -0700

    added more pokemon

--- services/pokemon-api.js
+++ services/pokemon-api.js
@@ -1,4 +1,4 @@
-const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex?page=1&perPage=155';
 
 export async function getPokemon() {
     const response = await fetch(URL);
