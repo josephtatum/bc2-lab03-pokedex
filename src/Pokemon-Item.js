c379889c17b1c685dc3da653cc2bc851bd11f16b
Joseph Tatum <josephtatum@gmail.com>
2019-11-04 14:23:29 -0800

    added links to tiles and add hover effect

--- src/Pokemon-Item.js
+++ src/Pokemon-Item.js
@@ -8,8 +8,10 @@ export class PokemonItem extends Component {
         
         return /*html */`
         <li class="pokemon-tile" style="background-color: ${pokemon.color_1}; background: linear-gradient(${pokemon.color_1}, ${pokemon.color_2});">
-            <img class="pokemon-img" src="${pokemon.url_image}">
-            <h2>${pokemon.id}:<br>${pokemon.pokemon}</h2>
+            <a href="${pokemon.pokedex}" target="_blank">
+                <img class="pokemon-img" src="${pokemon.url_image}">
+                <h2>${pokemon.id}:<br>${pokemon.pokemon}</h2>
+            </a>
         </li>
 `;
     }
